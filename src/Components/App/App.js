84c83ca3a2cf05f796bfc66eba3c69cc11d0786d
KarolinaEwa <gorska.karoliewa@gmail.com>
2020-06-24 00:19:35 +0300

    jamming project started

---
+++ src/Components/App/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './App.css';
+import { SearchBar } from '../SearchBar/SearchBar'
+import { SearchResults } from '../SearchResults/SearchResults';
+import { Playlist } from '../Playlist/Playlist';
+import { TrackList } from '../TrackList/TrackList';
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id:1}, 
+      {name: 'name2', artist: 'artist2', album: 'album2', id:2}, {name: 'name3', artist: 'artist3', album: 'album3', id:3}],
+      playlistName: 'Best Songs Ever',
+      playlistTracks: [{name: 'Poker Face', artist: 'Lady Gaga', album: 'Facts', id:4}, {name: 'Roar', artist: 'Kate Perry', album: 'Last night', id:5}, 
+      {name: 'Million', artist: 'Kate Melua', album: "Very first", id:6}]
+    }
+    this.addTrack = this.addTrack.bind(this)
+    this.removeTrack = this.removeTrack.bind(this)
+    this.updatePlaylistName = this.updatePlaylistName.bind(this)
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    } 
+    tracks.push(track)
+    
+    this.setState({
+      playlistTracks: tracks }) 
+  }
+
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks
+    tracks = tracks.filter(currenTrack => currenTrack.id !== track.id)
+    this.setState({
+      playlistTracks: tracks
+    })
+  }
+
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div class="App">
+          <SearchBar />
+
+          <div class="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} 
+                           onAdd={this.addTrack}/>
+            <Playlist playlistName={this.state.playlistName} 
+                      playlistTracks={this.state.playlistTracks}
+                      onRemove={this.removeTrack}
+                      onNameChange={this.updatePlaylistName}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+ 
+export default App;
---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './SearchBar.css'
+
+
+export class SearchBar extends React.Component {
+    render() {
+        return (
+            <div className="SearchBar">
+                <input placeholder="Enter A Song, Album, or Artist" />
+                <button class="SearchButton">SEARCH</button>
+            </div>
+        )
+    }
+
+}
